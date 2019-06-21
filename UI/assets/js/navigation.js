8167fa7fb5b513e3c8831e87f0756572b77e127a
Funmi Olaiya <funmiolaiya9@gmail.com>
2019-06-21 11:06:07 +0100

    chore(UIFiles): Add and edit some UI files
    - add pictures for the view properties and specific property pages
    - add js codes for the navigation sidebars
    - add icons for the user dashboard
    - design specific property page(prop1) with html and css

--- UI/assets/js/navigation.js
+++ UI/assets/js/navigation.js
@@ -7,3 +7,32 @@ function openNav1() {
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
   }
+
+  function Password() {
+    const x = document.getElementById("pwd");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
+  
+    
+  function myFunction() {
+    const x = document.getElementById("MyHeader");
+    if (x.className === "header-right") {
+      x.className += " responsive";
+    } else {
+      x.className = "header-right";
+    }
+  }
+  
+  function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+  
+   
