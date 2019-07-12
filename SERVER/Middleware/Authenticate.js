42b4bbc8c434820faeca98b80c652a021a195190
Funmi Olaiya <funmiolaiya9@gmail.com>
2019-07-13 00:48:56 +0100

    feat(postProperty): An endpoint for post property
    - create a controller [endpoint] for post-property
    - create validations to handle the endpoint
    - create a route to access the endpoint
    - write tests for the sign in controller
    - refactor the authentication if and return statement
    - refactor some lines of code [Usercontroller] and [Validateusers]
    - update travis configuration
    - update the JWT_TOKEN in the env file
    - update readMe badges
    
    [Delivers #167265642]

--- SERVER/Middleware/Authenticate.js
+++ SERVER/Middleware/Authenticate.js
@@ -7,24 +7,23 @@ class Auth {
     if (!token) {
       return res.status(400).json({
         status: '400',
-        error: 'Token is not provided',
+        error: 'Token is invalid or not provided!',
       });
     }
     try {
       const decoded = jwt.verify(token, process.env.SECRET_KEY);
       const rows = [decoded.userId];
+      /* istanbul ignore if */
       if (!rows) {
         return res.status(400).json({
-          data:
-          [{
-            status: 400,
-            error: 'The token you provided is invalid',
-          }],
+          status: '400',
+          error: 'Token is invalid or not provided!',
         });
       }
       return next();
     } catch (error) {
-      return res.status(400).send(error);
+      // return res.status(400)
+      //   .json(error);
     }
   }
 }
--- SERVER/Routes/prefixRoute.js
+++ SERVER/Routes/prefixRoute.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import userRoute from './userRoutes';
+import propertyRoute from './propertyRoute';
 
 const router = express.Router();
 
 router.use('/api/v1/auth', userRoute);
+router.use('/api/v1', propertyRoute);
 
 export default router;
