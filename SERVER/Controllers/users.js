9b92005ffb715ad42e16142b7dc1d8e694668b9d
Funmi Olaiya <funmiolaiya9@gmail.com>
2019-07-13 23:46:02 +0100

    chore(serverFiles): fix and update failing grader tests

--- SERVER/Controllers/users.js
+++ SERVER/Controllers/users.js
@@ -1,23 +1,22 @@
-import moment from 'moment';
 import db from '../DBconfig/index';
 import Helper from '../Middleware/Helper';
 
 class Usercontroller {
   static async signUp(req, res) {
     const createQuery = `INSERT INTO
-      users (email, first_name, last_name, password, phone_number, address, registered)
-      VALUES($1, $2, $3, $4, $5, $6, $7)
+      users (email, first_name, last_name, password, phone_number, address)
+      VALUES($1, $2, $3, $4, $5, $6)
       returning *`;
     const {
       email, first_name, last_name, password, phone_number, address,
     } = req.body;
     const hashPassword = Helper.hashPassword(password);
     const values = [email, first_name, last_name, hashPassword, phone_number,
-      address, moment(new Date())];
+      address];
     try {
       const { rows } = await db.query(createQuery, values);
       const token = Helper.generateToken(rows[0].id);
-      return res.status(201).json({ status: 'success', token, data: rows[0] });
+      return res.status(201).json({ status: 'success', data: { token, rows } });
     } catch (error) {
       /* istanbul ignore else */
       if (error.routine === '_bt_check_unique') {
--- db.js
+++ db.js
@@ -21,8 +21,7 @@ const createUsersTable = () => {
         last_name VARCHAR(50) NOT NULL,
         password VARCHAR(128) NOT NULL,
         phone_number VARCHAR(50),
-        address VARCHAR(100) NOT NULL,
-        registered TIMESTAMP
+        address VARCHAR(100) NOT NULL
       )`;
 
   pool.query(queryText)
