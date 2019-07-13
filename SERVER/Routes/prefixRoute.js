1facf56c46cd0afde261c842e8b4ad3ce7a8c3b6
Funmi Olaiya <funmiolaiya9@gmail.com>
2019-07-13 16:38:01 +0100

    fix(heroku): fix failing heroku url

--- SERVER/Routes/prefixRoute.js
+++ SERVER/Routes/prefixRoute.js
@@ -4,9 +4,6 @@ import propertyRoute from './propertyRoute';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.status(200).json({ message: 'Welcome to PropertyPro-Lite' });
-});
 router.use('/api/v1/auth', userRoute);
 router.use('/api/v1', propertyRoute);
 
