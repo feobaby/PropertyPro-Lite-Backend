1b3242bd6d2680221804c8ddab04937dd37e3503
Funmi Olaiya <funmiolaiya9@gmail.com>
2019-07-13 15:49:35 +0100

    chore(procfile): add procfile-heroku

--- SERVER/Routes/prefixRoute.js
+++ SERVER/Routes/prefixRoute.js
@@ -4,6 +4,9 @@ import propertyRoute from './propertyRoute';
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to PropertyPro-Lite' });
+});
 router.use('/api/v1/auth', userRoute);
 router.use('/api/v1', propertyRoute);
 
