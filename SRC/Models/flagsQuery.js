d7a3a7d49bc1316e911420d63145493839a94826
Funmilayo E. Olaiya <olufunmilayo335@gmail.com>
2020-03-21 23:10:32 +0100

    delete unused file

--- SRC/Models/flagsQuery.js
+++
@@ -1,5 +0,0 @@
-// eslint-disable-next-line import/prefer-default-export
-export const flagPropertyQuery = `INSERT INTO
-Flags (user_id, property_id, created_on, reason, description)
-VALUES($1, $2, $3, $4, $5)
-returning *`;
